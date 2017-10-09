da211a1e02bf76d35c2e56deb0a46b3271e2e2d3
Javier Aranda <javierarandacaro@gmail.com>
2017-10-09 10:37:00 +0200

    Añadido autor y version

--- app/src/main/java/com/example/loginlinearlayout/SplashActivity.java
+++ app/src/main/java/com/example/loginlinearlayout/SplashActivity.java
@@ -3,6 +3,11 @@ package com.example.loginlinearlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author Francisco Javier Aranda
+ * @version 1.0
+ */
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
